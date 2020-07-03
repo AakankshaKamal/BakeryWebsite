c3c4eb95b9e2e7627b19717e5bb936ba7a47c880
AakankshaKamal <58944717+AakankshaKamal@users.noreply.github.com>
2020-07-03 10:05:57 +0530

    updated form page

--- contact.php
+++ contact.php
@@ -1,8 +1,37 @@
+<!DOCTYPE html>
 <html>
+<head>
+	<title>formdetails</title>
+</head>
 <body>
 
-Welcome <?php echo $_POST["fname"]; ?><br>
-Your age is: <?php echo $_POST["age"]; ?>
+
+<?php 
+$lname=$fname = $country = $comment ="";
+
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+
+$fname=testinput($_POST["firstname"]);
+$lname=testinput($_POST["lastname"]);
+$country=testinput($_POST["country"]);
+$comment=testinput($_POST["subject"]);
+
+}
+
+function testinput($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+
+?>
+ 
+ 
 
 </body>
 </html>
